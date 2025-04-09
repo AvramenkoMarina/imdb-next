cca8b51410793fc875452dd98cfe196c3f0a270e
Maryna <marina.avramenko123@gmail.com>
2025-04-09 20:02:53 +0300

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,19 @@
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/header'
+import Header from '@/components/Header'
 import Providers from './Providers'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-	title: 'IMdb clone',
+	title: 'IMDb clone',
 	description: 'This is a movie database clone',
 }
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={inter.className}>
 				<Providers>
 					<Header />
 					{children}
