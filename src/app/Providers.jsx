624f203ae6c834023572a7f73c164c03564d3b8e
Maryna <marina.avramenko123@gmail.com>
2025-04-09 19:28:21 +0300

    add provider

---
+++ src/app/Providers.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+function Providers({ children }) {
+	return (
+		<ThemeProvider defaultTheme='system' attribute='class'>
+			<div className=''>{children}</div>
+		</ThemeProvider>
+	)
+}
+
+export default Providers
